42149cec7e2818d7b9fa0e41cb153eb3b5897c34
robinraja-003 <106648838+robinraja-003@users.noreply.github.com>
2023-11-09 12:07:43 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,8 +3,10 @@ class Fibonacci
 		public static void main(String args[])
 	{
 		int n1=0;
+		System.out.println(n1);
 		int n2=1;
-		for (int i=0;i<=10;i++)
+		System.out.println(n2);
+		for (int i=2;i<10;i++)
 			{
 				int n3=n1+n2;
 				System.out.println(n3);
